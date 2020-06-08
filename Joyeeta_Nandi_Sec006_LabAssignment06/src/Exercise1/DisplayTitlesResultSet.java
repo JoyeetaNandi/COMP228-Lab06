bbf04e3ff0e1606ee9c8b8817c05dda7bd4301da
JoyeetaNandi <joyeeta_nandi2005@yahoo.com>
2020-06-07 21:25:07 -0400

    Java Programming Lab Assignment -6

---
+++ Joyeeta_Nandi_Sec006_LabAssignment06/src/Exercise1/DisplayTitlesResultSet.java
@@ -0,0 +1,59 @@
+package Exercise1;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+public class DisplayTitlesResultSet {
+   public static void main(String args[]) {
+	  //final String DATABASE_URL = "jdbc:oracle:thin:@oracle1.centennialcollege.ca:1521:SQLD";
+	 final String DATABASE_URL = "jdbc:oracle:thin:@oracle1.centennialcollege.ca:1521:SQLD";  
+	  // final String DATABASE_URL = "jdbc:oracle:thin:@199.212.26.208:1521:SQLD";
+	   final String SELECT_QUERY =                            
+	      "SELECT ISBN, Title FROM titles WHERE EditionNumber BETWEEN 6 AND 10 ORDER BY ISBN";
+
+	   // use try-with-resources to connect to and query the database
+	   try (                                                        
+	      Connection connection = DriverManager.getConnection(DATABASE_URL, "COMP214_F19_zor_47", "password");                     
+         Statement statement = connection.createStatement();       
+         ResultSet resultSet = statement.executeQuery(SELECT_QUERY)
+         ) 
+           {
+
+         // get ResultSet's meta data
+         ResultSetMetaData metaData = resultSet.getMetaData();
+         int numberOfColumns = metaData.getColumnCount();     
+         
+         System.out.printf("Titles Table Result Set from Books Database:%n%n");
+
+         // display the names of the columns in the ResultSet
+         for (int i = 1; i <= numberOfColumns; i++) {
+            System.out.printf("%-8s\t", metaData.getColumnName(i));
+         }
+         System.out.println();
+         
+         // display query results
+         while (resultSet.next()) {
+            for (int i = 1; i <= numberOfColumns; i++) {
+               System.out.printf("%-8s\t", resultSet.getObject(i));
+            }
+            System.out.println();
+         } 
+      }
+      catch (SQLException sqlException) {
+         //sqlException.printStackTrace();
+    	  if(sqlException.getErrorCode() == 1017){
+         	 System.out.println("Username or password invalid");
+          }
+           if(sqlException.getErrorCode() == 17002){
+      	 System.out.println("Invalid URL");
+      }
+      }                                                   
+   } 
+} 
+
+
+
+ 
---
+++ Joyeeta_Nandi_Sec006_LabAssignment06/src/Exercise2/JdbcAuthersRowSetTest.java
@@ -0,0 +1,60 @@
+package Exercise2;
+// Fig. 24.29: JdbcRowSetTest.java
+// Displaying the contents of the Authors table using JdbcRowSet.
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import javax.sql.rowset.JdbcRowSet;    
+import javax.sql.rowset.RowSetProvider;
+
+public class JdbcAuthersRowSetTest {
+   // JDBC driver name and database URL  
+	private static final String DATABASE_URL = "jdbc:oracle:thin:@oracle1.centennialcollege.ca:1521:SQLD"; 
+	//private static final String DATABASE_URL = "jdbc:oracle:thin:@199.212.26.208:1521:SQLD";
+   private static final String USERNAME = "COMP214_F19_zor_47";
+   private static final String PASSWORD =  "password";
+   
+   public static void main(String args[]) {
+      // connect to database books and query database
+      try (JdbcRowSet rowSet = RowSetProvider.newFactory().createJdbcRowSet())            
+          {
+
+         // specify JdbcRowSet properties 
+         rowSet.setUrl(DATABASE_URL);                            
+         rowSet.setUsername(USERNAME);                           
+         rowSet.setPassword(PASSWORD);                           
+         rowSet.setCommand("SELECT authorID, firstName, lastName FROM authors WHERE authorid > 3 ORDER BY firstName"); // set query
+         rowSet.execute(); // execute query                      
+
+         // process query results
+         ResultSetMetaData metaData = rowSet.getMetaData();
+         int numberOfColumns = metaData.getColumnCount();
+         System.out.printf("Authors Table Row Set from Books Database:%n%n");
+
+         // display rowset header
+         for (int i = 1; i <= numberOfColumns; i++) {
+            System.out.printf("%-8s\t", metaData.getColumnName(i));
+         }
+         System.out.println();
+         
+         // display each row
+         while (rowSet.next()) {
+            for (int i = 1; i <= numberOfColumns; i++) {
+               System.out.printf("%-8s\t", rowSet.getObject(i));
+            }
+            System.out.println();
+         } 
+      }
+      catch (SQLException sqlException) {
+        // sqlException.printStackTrace();
+         if(sqlException.getErrorCode() == 1017){
+        	 System.out.println("Username or password invalid");
+         }
+          if(sqlException.getErrorCode() == 17002){
+     	 System.out.println("Invalid URL");
+     }
+    	  //System.out.println(sqlException.getErrorCode());
+         System.exit(1);
+      } 
+   } 
+}
+
